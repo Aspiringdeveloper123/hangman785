558678afe5e5ee21ad2536c975675ea3b8e796f7
Pareena Shah <pareena10@hotmail.co.uk>
2024-04-29 16:29:00 +0100

    Adds meaningful naming to code

--- milestone_2.py
+++ milestone_2.py
@@ -2,18 +2,18 @@ import random
 
 # this file will contain the code for the first milestone
 # create list of your 5 favourite fruits
-word_list = ["Strawberry", "Watermelon", "Mango", "Pineapple", "Kiwi"]
-print(word_list)
+list_of_favourite_fruits = ["Strawberry", "Watermelon", "Mango", "Pineapple", "Kiwi"]
+print(list_of_favourite_fruits)
 
-word = random.choice(word_list)
-print(word)
+randomly_generated_fruit = random.choice(list_of_favourite_fruits)
+print(randomly_generated_fruit)
 
 #ask the user for input and check that the input is a single character
 #also need to check that only alphabetical characters are provided by the user
 #create conditional checks that must be passed before the input can be accepted
 
-guess = input("Enter a single letter: ")
-if (len(guess) == 1 and guess.isalpha() == True):
+guess_the_letter = input("Enter a single letter: ")
+if (len(guess_the_letter) == 1 and guess_the_letter.isalpha() == True):
     print("Good guess!")
 else:
     print("Oops! That is not a valid input")
