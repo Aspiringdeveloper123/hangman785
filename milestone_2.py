6e4321b7158699f0e27d81f33465665994af25b1
Pareena Shah <pareena10@hotmail.co.uk>
2024-04-30 12:23:32 +0100

    Optimised code via refactoring

--- milestone_2.py
+++ milestone_2.py
@@ -8,12 +8,19 @@ print(list_of_favourite_fruits)
 randomly_generated_fruit = random.choice(list_of_favourite_fruits)
 print(randomly_generated_fruit)
 
-#ask the user for input and check that the input is a single character
-#also need to check that only alphabetical characters are provided by the user
-#create conditional checks that must be passed before the input can be accepted
-
 guess_the_letter = input("Enter a single letter: ")
-if (len(guess_the_letter) == 1 and guess_the_letter.isalpha() == True):
+
+def validate_input(user_input):
+    return len(guess_the_letter) == 1 and guess_the_letter.isalpha()
+
+if validate_input(guess_the_letter):
     print("Good guess!")
-else:
-    print("Oops! That is not a valid input")
+
+else: 
+    print(" Oops! That is not a valid input")
+
+#if len(guess_the_letter) == 1 and guess_the_letter.isalpha(): 
+    #print("Good guess!")
+#else:
+    #print("Oops! That is not a valid input")
+
