f1edf79c55417f91acff012ebe3cba374d67084e
Pareena Shah <pareena10@hotmail.co.uk>
2024-05-07 13:31:48 +0100

    Check if guess is in the word & create functions to run the checks

--- milestone_3.py
+++ milestone_3.py
@@ -6,13 +6,30 @@ print(list_of_favourite_fruits)
 randomly_generated_fruit = random.choice(list_of_favourite_fruits)
 print(randomly_generated_fruit)
 
-while True:  # let the code run continuosly 
-    guess_the_letter = input("Enter a single letter: ")
 
-    if len(guess_the_letter) == 1 and guess_the_letter.isalpha():
-        break
+def check_guess(guess_the_letter):
+ 
+    if guess_the_letter in randomly_generated_fruit.lower():
+        print(f'"Good guess! {guess_the_letter} is in the word")')
+    
+    else:
+        print(f'"Sorry, {guess_the_letter} is not in the word. Try again")')
 
-    else:   
-        print("Invalid letter. Please, enter a single alphabetical character")
 
-print("Good guess")
+def ask_for_input():
+    while True:  # let the code run continuosly 
+        guess_the_letter = input("Guess the letter: ").lower() #.lower() applied directly to the input function
+
+        if len(guess_the_letter) == 1 and guess_the_letter.isalpha():
+            break
+
+        else:   
+            print("Invalid letter. Please, enter a single alphabetical character")
+    check_guess(guess_the_letter)
+
+    print("Good guess") #this can't be placed after the "break" as break means the loop has already exited and jumps to the next line of code
+
+ask_for_input()
+
+
+
