2a57eb64cd0be74656b1138f6c2a17542b678074
Pareena Shah <pareena10@hotmail.co.uk>
2024-05-07 13:38:11 +0100

    Added meaningful naming to functions

--- milestone_3.py
+++ milestone_3.py
@@ -7,7 +7,7 @@ randomly_generated_fruit = random.choice(list_of_favourite_fruits)
 print(randomly_generated_fruit)
 
 
-def check_guess(guess_the_letter):
+def check_guess_in_word(guess_the_letter):
  
     if guess_the_letter in randomly_generated_fruit.lower():
         print(f'"Good guess! {guess_the_letter} is in the word")')
@@ -16,7 +16,7 @@ def check_guess(guess_the_letter):
         print(f'"Sorry, {guess_the_letter} is not in the word. Try again")')
 
 
-def ask_for_input():
+def ask_for_valid_user_input():
     while True:  # let the code run continuosly 
         guess_the_letter = input("Guess the letter: ").lower() #.lower() applied directly to the input function
 
@@ -25,11 +25,11 @@ def ask_for_input():
 
         else:   
             print("Invalid letter. Please, enter a single alphabetical character")
-    check_guess(guess_the_letter)
+    check_guess_in_word(guess_the_letter)
 
     print("Good guess") #this can't be placed after the "break" as break means the loop has already exited and jumps to the next line of code
 
-ask_for_input()
+ask_for_valid_user_input()
 
 
 
