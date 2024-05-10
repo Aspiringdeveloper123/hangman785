a9151b05daed1764f9c1514a8d499a9421ccc60d
Pareena Shah <pareena10@hotmail.co.uk>
2024-05-10 14:55:33 +0100

    Added docstrings

--- milestone_4.py
+++ milestone_4.py
@@ -12,30 +12,42 @@ class Hangman():
     Attributes:
         randomly_generated_fruit = word to be guessed which is randomly generated from the "list of fruits" list
         word_guessed(list) = a list of the letters for the word
-        num_letters(int) = number of unique letters inthe word that has not been guessed yet  
+        num_letters(int) = number of unique letters in the word that has not been guessed yet  
         list_of_fruits(list) = list of fruits
         list_of_guesses(list) = list of guesses that the user has already tried
         num_lives(int) = number of lives at the start of the game
 
     '''
-    def __init__(self, randomly_generated_fruit, num_letters, list_of_fruits, list_of_guesses=[], num_lives=5 ):
+    def __init__(self, randomly_generated_fruit, word_guessed, num_letters, list_of_fruits, list_of_guesses=[], num_lives=5 ):
       
         self.randomly_generated_fruit = randomly_generated_fruit
+        self.word_guessed = word_guessed
         self.num_letters = num_letters   
         self.list_of_fruits = list_of_fruits
-        self.list_of_guesses = list_of_guesses
+        self.list_of_guesses = list_of_guesses 
         self.num_lives = num_lives
 
 
         def initialise_word_guessed(self):
-             # create/ initialise a list of underscores, with each underscore representing a letter in the word to be guessed
              # use self so that the "word_guessed" attriute can be accessed by other methods within the class (e.g _check_guess_in_word)
-             # remove attribute in the __init__ method
             self.word_guessed  = ["_"] * len(self.randomly_generated_fruit) 
+        '''
+            This function initialises a list of underscores for the chosen word
+
+            Returns:
+                A list created with each underscore representing a letter in the word to be guessed
+        
+        '''
 
         def _check_guess_in_word(self, guess):
+        
+            '''
+            This function is used to check  if the guessed letter (`guess`) is present in the word to be guessed
 
-            #check if the guessed letter (`guess`) is in the word to be guessed
+            Returns: 
+                str: "Good guess! {guess} is in the word" if the letter is in the word and updates the corresponding position in the word_guessed list with the correct letter  
+            '''
+        
             if guess in self.randomly_generated_fruit.lower():
                 print(f'"Good guess! {guess} is in the word")')
        
@@ -56,19 +68,42 @@ class Hangman():
                 self.num_letters -= 1
 
         def _ask_for_valid_user_input(self):
+
+            '''
+            This function is used ask for valid user input
+
+            Returns: 
+                 If input is valid, the letter will be appended to the list_of_guesses
+            '''
+
             while True:  # let the code run continuosly 
                 guess = input("Guess the letter: ").lower() #.lower() applied directly to the input function
                 if validate_guess and not already_guessed:
                     self.check_guess_in_word(guess)  #then runs the code in the method - "check_guess_in_word"
-                    self.list_of_guesses.append(guess)
+                    self.list_of_guesses.append(guess) #captures guesses that are not already in the list hence it says "and not"
                 break   # to break out of the loop
         
         def validate_guess(self, guess):   #need to condition to explain what happens when this condition is true (seen above)
+            
+            '''
+            This function is used to validate the guess, to ensure it's a single alphabetical character
+
+            Returns: 
+                str: "Invalid letter. Please, enter a single alphabetical character" 
+            '''
+            
             if len(guess) != 1 or not guess.isalpha():
                     print("Invalid letter. Please, enter a single alphabetical character")
 
-        def already_guessed(self,guess):     
+        def already_guessed(self,guess):  
+            
+            '''
+            This function is to let the user know they have already entered a letter tried previously
+
+            Returns: 
+                str: "You already tried that letter"
+            '''
             if guess in self.list_of_guesses:
                     print( "You already tried that letter")
 
-            ask_for_valid_user_input()
+        ask_for_valid_user_input()
