3db76590a2be8a43228764e960f865c427e34b65
Pareena Shah <pareena10@hotmail.co.uk>
2024-05-13 12:13:29 +0100

    Adding changes to code

--- milestone_5.py
+++ milestone_5.py
@@ -1,7 +1,5 @@
 import random
 list_of_fruits = ["Strawberry", "Watermelon", "Mango", "Pineapple", "Kiwi"]
-print(list_of_fruits)
-
 randomly_generated_fruit = random.choice(list_of_fruits)
 print(randomly_generated_fruit)
 
@@ -18,16 +16,19 @@ class Hangman():
         num_lives(int) = number of lives at the start of the game
 
     '''
-    def __init__(self, randomly_generated_fruit, word_guessed, num_letters, list_of_fruits, list_of_guesses=[], num_lives=5 ):
+
+    
+    def __init__(self, list_of_fruits, list_of_guesses=[], num_lives=5 ):  #that game needs to run the player puts in
       
-        self.randomly_generated_fruit = randomly_generated_fruit
-        self.word_guessed = word_guessed
-        self.num_letters = num_letters   
+        self.randomly_generated_fruit = random.choice(list_of_fruits)
+        self.word_guessed = ["_"] * len(self.randomly_generated_fruit)   #class variable which has been assigned a value
+        self.num_letters = len(set(randomly_generated_fruit))  
         self.list_of_fruits = list_of_fruits
         self.list_of_guesses = list_of_guesses 
         self.num_lives = num_lives
 
-    def _play_game(self, list_of_fruits):
+
+    def _play_game(self):
         '''
         This function represents the start of the Hangman game
 
@@ -43,7 +44,9 @@ class Hangman():
             When there are no remaining letters to be guessed and the user's lives are more than 0, the user has won the game
         
         '''
-   
+
+        print(self.list_of_fruits)
+
     
         while True:
             if self.num_lives == 0:
@@ -57,24 +60,15 @@ class Hangman():
                 return "Congratulations. You won the game!"
                
   
-    def _initialise_word_guessed(self):
-         # use self so that the "word_guessed" attriute can be accessed by other methods within the class (e.g _check_guess_in_word)
-        self.word_guessed  = ["_"] * len(self.randomly_generated_fruit) 
-        return self.word_guessed
-    '''
-        This function initialises a list of underscores for the chosen word
-
-        Returns:
-            A list created with each underscore representing a letter in the word to be guessed
-        
-    '''
 
     def _check_guess_in_word(self, guess):   
         '''
-        This function is used to check  if the guessed letter (`guess`) is present in the word to be guessed
+        This function is used to check  if the guessed letter (`guess`) is present in the word to be guessed. 
 
         Returns: 
-            str: "Good guess! {guess} is in the word" if the letter is in the word and updates the corresponding position in the word_guessed list with the correct letter  
+            str: "Good guess! {guess} is in the word" if the letter is in the word and updates the corresponding position
+            in the word_guessed list with the correct letter  
+
         '''
         if guess in self.randomly_generated_fruit.lower():
             print(f'"Good guess! {guess} is in the word")')
@@ -88,55 +82,40 @@ class Hangman():
                     # update the corresponding position in the word_guessed list with the correct letter.
                     # the index variable tells us the position of the current letter in the word
                     self.word_guessed[index] = letter 
-                  
+            self.list_of_guesses.append(guess) #captures guesses that are not already in the list hence it says "and not"
+            self.num_letters -= 1
         else:
             self.num_lives -= 1
-            print("Sorry, {guess} is not in the word")
-            print("You have {num_lives} lives left")
-            self.num_letters -= 1
+            print(f"Sorry, {guess} is not in the word")
+            print(f"You have {self.num_lives} lives left")
+           
 
-    def _ask_for_valid_user_input(self):  #this is already been accessed in the play_game method so you don't need to call it again ask_for_valid_user_input()
-                                          # as this will make it a recursive function
+   # this is already been accessed in the play_game method so you don't need to call it again- ask_for_valid_user_input as this will make it a recursive function
+   
+    def _ask_for_valid_user_input(self):  
         '''
-        This function is used ask for valid user input
+        This function is used to ask for valid user input, ensuring it's a single alphabetical character and to check if the guess has already been made
 
-        Returns: 
-                If input is valid, the letter will be appended to the list_of_guesses
+        Returns: Nothing specific
+                
         '''
         while True:  # let the code run continuosly 
             guess = input("Guess the letter: ").lower() #.lower() applied directly to the input function
-            if self.validate_guess(guess) and not self.already_guessed(guess):  #calling a method of an object where `self` is the object
-                self.check_guess_in_word(guess) #then runs the code in the method - "check_guess_in_word"
-                self.list_of_guesses.append(guess) #captures guesses that are not already in the list hence it says "and not"
-                break   # to break out of the loop
 
-    def validate_guess(self, guess):   #need to condition to explain what happens when this condition is true (seen above)         
-        '''
-        This function is used to validate the guess, to ensure it's a single alphabetical character
-
-        Returns: 
-            str: "Invalid letter. Please, enter a single alphabetical character" 
-        '''   
-        if len(guess) != 1 or not guess.isalpha():
+            if len(guess) != 1 or not guess.isalpha():
                 print("Invalid letter. Please, enter a single alphabetical character")
-                return False
-            return True
-
-    def already_guessed(self,guess):            
-        '''
-        This function is to let the user know they have already entered a letter tried previously
-
-        Returns: 
-            str: "You already tried that letter"
-        '''
-        if guess in self.list_of_guesses:
-             print( "You already tried that letter")
-
-
+            elif guess in self.list_of_guesses:
+                print( "You already tried that letter")
+            else:
+                self._check_guess_in_word(guess) #then runs the code in the method - "check_guess_in_word"
+                break
+           
+ 
 num_lives = 5
-game = Hangman(randomly_generated_fruit, [], len(set(randomly_generated_fruit)), list_of_fruits, [], num_lives=5)
-print(game._play_game(list_of_fruits))
-print(game._initialise_word_guessed())
+list_of_fruits = ["Strawberry", "Watermelon", "Mango", "Pineapple", "Kiwi"]
+game = Hangman(list_of_fruits)   #instance of the class, to call the class 
+game._play_game()
+
 
 
 
